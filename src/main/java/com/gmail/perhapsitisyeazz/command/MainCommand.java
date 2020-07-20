b8b558f7ac02437d69d6246dd99c8faf6355d023
YeaZz <perhapsitisyeazz@gmail.com>
2020-07-20 22:23:52 +0200

    Update

---
+++ src/main/java/com/gmail/perhapsitisyeazz/command/MainCommand.java
@@ -0,0 +1,55 @@
+package com.gmail.perhapsitisyeazz.command;
+
+import com.moderocky.mask.command.Commander;
+import com.moderocky.mask.template.WrappedCommand;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainCommand extends Commander<CommandSender> implements WrappedCommand {
+
+
+	@Override
+	protected CommandImpl create() {
+		return command("mail");
+	}
+
+	@Override
+	public CommandSingleAction<CommandSender> getDefault() {
+		return null;
+	}
+
+	@Override
+	public @NotNull List<String> getAliases() {
+		return new ArrayList<>();
+	}
+
+	@Override
+	public @NotNull String getUsage() {
+		return "/" + getCommand();
+	}
+
+	@Override
+	public @NotNull String getDescription() {
+		return "This is the main command";
+	}
+
+	@Override
+	public @Nullable String getPermission() {
+		return null;
+	}
+
+	@Override
+	public @Nullable String getPermissionMessage() {
+		return null;
+	}
+
+	@Override
+	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+		return execute(sender, args);
+	}
+}
