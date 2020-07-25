12222a7009265a5ecc856814e23abe7f63c8fa71
YeaZz <perhapsitisyeazz@gmail.com>
2020-07-25 02:04:59 +0200

    Update

--- src/main/java/com/gmail/perhapsitisyeazz/manager/SendMail.java
+++ src/main/java/com/gmail/perhapsitisyeazz/manager/SendMail.java
@@ -1,11 +1,23 @@
 package com.gmail.perhapsitisyeazz.manager;
 
+import com.gmail.perhapsitisyeazz.util.Data;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 public class SendMail {
 
 	public static void sendMail(Player sender, OfflinePlayer target, String message) {
-		sender.sendMessage(message);
+		JsonObject object = Data.getJsonObject(target);
+		JsonArray array = object.getAsJsonArray("EmailList");
+		for (JsonElement mails : array) {
+			JsonArray finalArray = mails.getAsJsonArray();
+			if (finalArray.get(1).getAsString().equals(sender.getUniqueId().toString()) && finalArray.get(2).getAsString().equals(message)) {
+				sender.sendMessage("Same message already sent.");
+				return;
+			}
+		}
 	}
 }
