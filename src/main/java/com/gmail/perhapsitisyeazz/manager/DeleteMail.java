54eef1d0410ff5095dbe822775e94dc6e3cec547
YeaZz <perhapsitisyeazz@gmail.com>
2020-07-26 17:04:27 +0200

    Update

--- src/main/java/com/gmail/perhapsitisyeazz/manager/DeleteMail.java
+++ src/main/java/com/gmail/perhapsitisyeazz/manager/DeleteMail.java
@@ -1,10 +1,20 @@
 package com.gmail.perhapsitisyeazz.manager;
 
+import com.gmail.perhapsitisyeazz.util.Data;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import org.bukkit.entity.Player;
 
 public class DeleteMail {
 
 	public static void deleteMail(Player sender, Integer integer) {
-
+		JsonObject object = Data.getJsonObject(sender);
+		JsonArray array = object.getAsJsonArray("EmailList");
+		int size = array.size();
+		if (integer <= 0 && integer > size) {
+			sender.sendMessage("This mail doesn't exist.");
+			return;
+		}
+		array.remove(integer);
 	}
 }
