5b67f51f395874aed0ecd84fb2d8a6af02b04573
YeaZz <perhapsitisyeazz@gmail.com>
2020-07-27 20:22:12 +0200

    Update

--- src/main/java/com/gmail/perhapsitisyeazz/manager/DeleteMail.java
+++ src/main/java/com/gmail/perhapsitisyeazz/manager/DeleteMail.java
@@ -15,6 +15,13 @@ public class DeleteMail {
 			sender.sendMessage("This mail doesn't exist.");
 			return;
 		}
+		sender.sendMessage("Are you sure ?");
+		confirmDeleteMail(sender, integer);
+	}
+
+	public static void confirmDeleteMail(Player sender, Integer integer) {
+		JsonObject object = Data.getJsonObject(sender);
+		JsonArray array = object.getAsJsonArray("EmailList");
 		array.remove(integer);
 	}
 }
